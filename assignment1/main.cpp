21a94e8973af77365c6944039ed6d14728302f77
Yonghye Kwon <developer.0hye@gmail.com>
2019-03-08 12:48:54 +0900

    Create main.cpp

---
+++ assignment1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string WARNINGS[] = {
+"Must contain at least 6 more characters.",
+"Must contain at least 1 digit.",
+"Must contain at least 1 lowercase English character.",
+"Must contain at least 1 uppercase English character.",
+"Must contain at least 1 special character."};
+
+int main()
+{
+	string password;
+	cin >> password;
+
+	// Complete the code below.
+	
+
+    return 0;
+}
